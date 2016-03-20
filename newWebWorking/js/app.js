ace90d3e6a446d835107241db6c6d8e112488e7d
Mohammad Faizan <mfaizanattique@gmail.com>
2016-03-20 09:30:43 +0400

    added-new-tested-web
    added working website for testing example

---
+++ newWebWorking/js/app.js
@@ -0,0 +1,108 @@
+﻿(function () {
+    var app = angular.module('appOne', []);
+    app.controller('porfolioController', function ($scope, behFactory,behProject) {
+
+        //this.person = [{ name: 'Faizan', profession: 'Web developer', salary: [1500, 1650, 1900] }, { name: 'Farrukh', profession: 'Team Lead Operations' }];
+
+        //this.behance = behFactory.async();
+        //console.log(this.behance);
+
+        var promise = behFactory.async();
+        promise.then(
+          function (behanceData) {
+              $scope.behanceProjects = behanceData.projects;
+              console.log($scope.behanceProjects);
+              angular.forEach($scope.behanceProjects.covers, function (cover, index) {
+                  
+              });
+          },
+          function (errorData) {
+              console.log('failure loading Projects', errorData);
+          });
+
+        $scope.selectedProject=false;
+
+        $scope.getProject = function (obj) {
+            
+            //console.log(obj);
+
+            var selectedPromise = behProject.async(obj);
+            selectedPromise.then(function (behanceData) {
+                $scope.selectedProject = behanceData.project;
+                //console.log(behanceData.project);
+                $("#portfolioModal1").modal('show');
+            }, function (errorData) { console.log('failure loading Project', errorData); });
+            
+            //angular.forEach($scope.behanceProjects, function (project, index) {
+            //    if (project.id == obj)
+            //    {
+            //        //alert(project.name);
+            //        $scope.selectedProject = project;
+            //        angular.forEach(project.covers, function (value, index) {
+            //            $scope.selectedProject.coverImage = value;
+            //        });
+            //        //$("#filterprojects").val(obj);
+            //        
+            //        return;
+            //    }
+            //});
+            
+        }
+
+    });
+    app.filter('imageSize', function () {
+        return function (items, field) {
+            var result = {};
+            angular.forEach(items, function (value, key) {
+                if (!value.hasOwnProperty(field)) {
+                    result[key] = value;
+                }
+            });
+            return result;
+        };
+    });
+    var user = 'faizanatiq';
+    var apiKey = 'DCOtD0PynrZPP0UXtHQWeRbbUExF7Ck6';
+    app.factory('behFactory', function ($http) {
+        var factory = {
+            async: function (page) {
+
+                var url = 'http://behance.net/v2/users/' + user + '/projects?api_key=' + apiKey + '&callback=JSON_CALLBACK';
+                var promise = $http.jsonp(url).error(function (response, status) {
+                    alert(status);
+                }).success(function (response, status) {
+                    //console.log(response.projects);
+                    //return response.projects;
+                }).then(function (response, status) {
+                    return response.data;
+                });
+                return promise;
+            }
+        };
+        return factory;
+
+    });
+
+    app.factory('behProject', function ($http) {
+        var factory = {
+            async: function (projectID) {
+             
+                
+                //var url = 'http://behance.net/v2/projects/project_id/projects?api_key=' + apiKey + '&callback=JSON_CALLBACK';
+                var url = 'http://www.behance.net/v2/projects/' + projectID + '?client_id=' + apiKey + '&callback=JSON_CALLBACK';
+                
+                var promise = $http.jsonp(url).error(function (response, status) {
+                    alert(status);
+                }).success(function (response, status) {
+                   //console.log(response.project);
+                    //return response.projects;
+                }).then(function (response, status) {
+                    return response.data;
+                });
+                return promise;
+            }
+        };
+        return factory;
+
+    });    
+})();
---
+++ newWebWorking/js/resumeBuilder.js
@@ -0,0 +1,96 @@
+﻿var skills = ["CSS", "HTML", "ASP.net", "C#", "Angular Js"];
+var contacts = { "phone": "+971563212715", "email": "mfaizanattique@gmail.com", "facebook": "facebook.com/muhammadfaizanatiq", "twitter": "@mfaizanattique" };
+var bio = {
+    "name": "Muhammad Faizan Atiq",
+    "role": "Front End Web developer",
+    "contact": contacts,
+    "pictureurl": "https://mir-s3-cdn-cf.behance.net/user/138/1932613.53b6ddbff3fba.jpg",
+    "message": "Welcome to my Career",
+    "skills": skills
+};
+
+var work = {
+    "position": [{
+        "position": "Web Developer and Designer",
+        "employers": "United Technical Services",
+        "years": "3 Years",
+        "city": "Abu Dhabi"
+    }, {
+        "position": "Software Engineer",
+        "employers": "MTBC",
+        "years": "1 Years",
+        "city": "Rawalpindi, Pakistan"
+    },
+    ]
+};
+
+var projects = {
+    "project": [{
+        "title": "Interactive Media Table Presentation",
+        "description": "The Project is an interactive demo for UTS Company & explains various of it's sectors interactively with touch interface. The project is focused on 7 principal activities of the company. Project is developed on Unity 3D game engine and the models are structured using Google Sketch up. The graphics for the project were mastered using Adobe packages like Photoshop and Illustrator. Scripting language used is C# and Scripting environment is Visual Studio 2013. Project uses third party plugin TouchScript by Interactive Lab for touch activities. TouchScript was customized for full the functional UI requirements",
+        "date": "November, 2015",
+        "images": ["https://media.licdn.com/media-proxy/ext?w=543&h=362&f=&hash=urEtVjZwhW6LIy%2B%2FyOnfBZl1kaU%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta9Er0Vinkhwfjw8177yE41y87UNCVordEGXyD3u0qYrdfyPqK8LYL7XyuVtHei8clABjffKgRGOzD5a-KYzrdNwi3cXld424ZxUBbFImi24"]
+    }, {
+        "title": "UTS BI Mobile Application",
+        "description": "The app provides the following real-time data from the Oracle ERP and quotation systems •	Sales, Revenue & margin – comparing years, quarters and months •	Sales & margin by agency over the years •	Sales & margin by customers over the years •	Export sales over the years with customer breakdown •	Sales backlog by month with detailed breakdown of agency/customer •	Review monthly reports",
+        "date": "3 Years",
+        "images": ["https://media.licdn.com/media-proxy/ext?w=543&h=407&f=&hash=asRW3LZ2hHgTslg7oHgGB94fBPI%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta9Er0Vinkhwfjw8177yE41y87UNCVordEGXyD3u0qYrdf3Xvf87ZfbCiuVQWLSsclABhevKhQDTjD5a9eN_pL4gkiJa3LY24ZxUBbFImi24"]
+    }
+    ]
+};
+
+
+var education = {
+    "schools": [
+        {
+            "name": "SZABIST",
+            "location": "Dubai, UAE",
+            "degree": "MS - Computer Science",
+            "majors": ["CS"],
+            "dates": "Expected-2016",
+            "url": "2"            
+        },
+        {
+            "name": "BIIT",
+            "Location": "Rawalpindi, Pakistan",
+            "degree": "BS - Information Technology",
+            "majors": ["Software Engineering"],
+            "dates": "2010",
+            "url": "4"            
+        },
+        {
+            "name": "FBISE",
+            "location": "Islamabad, Pakistan",
+            "degree": "HSSC",
+            "majors": ["General Science"],
+            "dates": "2006",
+            "url": "2"            
+        },
+        {
+            "name": "FBISE",
+            "location": "Islamabad, Pakistan",
+            "degree": "HSSC",
+            "majors": ["General Science"],
+            "dates": "2006",
+            "url": "2"
+        }
+    ], "OnlineCourses": [{
+        "title": "Gamification",
+        "school": "Coursera",
+        "date": "March 2015",
+        "url":""
+    },
+    {
+        "title": "Front-end Foundations",
+        "school": "Code School",
+        "date": "February 2015",
+        "url": ""
+    },
+    {
+        "title": "Shaping up with Angulat JS",
+        "school": "Code School",
+        "date": "March 2015",
+        "url": ""
+    }
+    ]
+};
